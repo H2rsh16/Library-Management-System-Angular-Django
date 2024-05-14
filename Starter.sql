d971f09b67145063cbfea507f07a8ef1be206eb4
Harsh Ovhal <135042421+H2rsh16@users.noreply.github.com>
2024-05-14 22:04:46 +0530

    Add files via upload

---
+++ Starter.sql
@@ -0,0 +1,14 @@
+create database librarymanagement;
+
+use librarymanagement;
+
+
+insert into library_books(bookname, bookdescription, bookcategory, authorname, quantity) values('Python', 'Python', 'Programming', 'Manisha Bharambe', 20),
+('C', 'C programming', 'Programming', 'Manisha Bharambe', 20),
+('Core Java', 'Java programming', 'Programming', 'Manisha Bharambe', 20),
+('Advance Java', 'Advance Java programming', 'Programming', 'Nikhil H. Deshpande', 20),
+('CPP', 'Advance C programming', 'Programming', 'Manisha Bharambe', 20),
+('PHP', 'Hypertext Preprocessor', 'Programming', 'Gajanan A. Deshmukh', 20),
+('Node JS', 'Javscript Framework(Node.Js)', 'Programming', 'Shivendu Bhushan', 20),
+('Angular JS', 'Javascript Framework(Angular.js)', 'Programming', 'Gajanan A. Deshmukh', 20),
+('Android Programming', 'Android Development', 'Programming', 'Kamil Ajmal Khan', 20);
